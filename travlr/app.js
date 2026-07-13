3f367bbc7ffd586e8b53055a894c0f117e6ebe9f
Andres T <andres.trujillo@snhu.edu>
2026-07-13 18:51:32 -0500

    Module 2 completed baseline

--- travlr/app.js
+++ travlr/app.js
@@ -11,7 +11,7 @@ var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
